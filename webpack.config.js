320f8ba97a0ad4f5706892a56790b90df60171d0
Ricardo Soares <botequilha@gmail.com>
2015-12-02 14:19:16 +0000

    Setup webpack build.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./app/app.js",
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ["es2015", "react"]
+      }
+    }]
+  }
+};
