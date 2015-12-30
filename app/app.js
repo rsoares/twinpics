2214fe94a1c33564f6de1d9184b8f8aec9c49fff
Ricardo Soares <botequilha@gmail.com>
2015-12-30 12:09:42 +0000

    Render app components in main app.js

--- app/app.js
+++ app/app.js
@@ -1,2 +1,14 @@
-import Board from './board/board.jsx';
 import './global.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './board/board.jsx';
+import Timer from './timer/timer.jsx';
+
+ReactDOM.render(
+  <Timer/>,
+  document.getElementById('timer')
+);
+ReactDOM.render(
+  <Board/>,
+  document.getElementById('board')
+);
