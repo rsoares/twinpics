25d32d6dbecc8913865724de6de03461c38d1c03
Ricardo Soares <botequilha@gmail.com>
2015-12-30 12:09:12 +0000

    Adds timer component and timer placeholder.

---
+++ app/timer/timer.jsx
@@ -0,0 +1,18 @@
+// The timer. Faster, faster!
+import React from 'react';
+
+class Timer extends React.Component {
+  constructor(props) {
+
+  };
+
+  render() {
+    return (
+      <div className=''>
+
+      </div>
+    );
+  }
+}
+
+export default Timer;
